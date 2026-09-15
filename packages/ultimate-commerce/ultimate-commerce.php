c68d7bf2175169639cab282b446b83ae108cf840
danielrichmonduk-droid <danielrichmonduk@gmail.com>
2026-09-15 16:16:10 +0100

    chore(release): bump Ultimate Commerce to 0.1.4

--- packages/ultimate-commerce/ultimate-commerce.php
+++ packages/ultimate-commerce/ultimate-commerce.php
@@ -3,7 +3,7 @@
  * Plugin Name: Ultimate Commerce
  * Plugin URI: https://badotter.io/ultimate-commerce
  * Description: A modular enhancement platform for WooCommerce storefronts, customer journeys and commerce operations.
- * Version: 0.1.3
+ * Version: 0.1.4
  * Requires at least: 6.6
  * Requires PHP: 8.1
  * Requires Plugins: woocommerce
@@ -15,7 +15,7 @@
 
 defined('ABSPATH') || exit;
 
-define('ULTIMATE_COMMERCE_VERSION', '0.1.3');
+define('ULTIMATE_COMMERCE_VERSION', '0.1.4');
 define('ULTIMATE_COMMERCE_SCHEMA_VERSION', '1');
 define('ULTIMATE_COMMERCE_FILE', __FILE__);
 define('ULTIMATE_COMMERCE_DIR', plugin_dir_path(__FILE__));
